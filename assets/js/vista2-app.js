8fb23233e1bd4a4d3bc02a037e0f6f421b51a753
pinalef <tierralavista@gmail.com>
2018-01-11 15:07:24 -0300

    actualizando archivo inicio sesion

--- assets/js/vista2-app.js
+++ assets/js/vista2-app.js
@@ -1,24 +1,57 @@
 $(document).ready(function(){
 
 	console.log(data);
-
+	//llenando contenedores con peliculas del genero Drama
+	for (var i = 0 ;i < data.Drama.length ; i++){
+		$('.main').append('<div class="wrapper vista-first">' +
+											' <div class="row">'+
+											'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
+											'<div class="view"> '+
+											'<img class= " " src="'+data.Drama[i].Poster +'" alt="">'+
+											'<div class="mask">'+
+											'<h2>'+ data.Drama[i].Title
+											+'</div>'
+											+'</h2>'
+											+'</div>' 
+											+'</div>' 
+											+'</div>'
+											+'</div>');
+			};
 
 	
-	for (var i = 0 ;i < data.Family.length ; i++){
-	
-$('.main').append('<div class="wrapper vista-first">' +
-									' <div class="row">'+
-									'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
-									'<div class="view"> '+
-									'<img class= " " src="'+data.Comedy[i].Poster +'" alt="">'+
-									'<h2>'+ data.Comedy[i].Title
-									+'</h2>'
-									+'</div>' 
-									+'</div>' 
-									+'</div>'
-									+'</div>');
-	};
+	//llenando contenedores con peliculas del genero Comedy
+	for (var i = 0 ;i < data.Comedy.length ; i++){
+		$('.main').append('<div class="wrapper vista-first">' +
+											' <div class="row">'+
+											'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
+											'<div class="view"> '+
+											'<img class= " " src="'+data.Comedy[i].Poster +'" alt="">'+
+											'<div class="mask">'+
+											'<h2>'+ data.Comedy[i].Title
+											+'</div>'
+											+'</h2>'
+											+'</div>' 
+											+'</div>' 
+											+'</div>'
+											+'</div>');
+			};
 
 	
+		//llenando contenedores con peliculas del genero Biography
+		for (var i = 0 ;i < data.Biography.length ; i++){
+			$('.main').append('<div class="wrapper vista-first">' +
+												' <div class="row">'+
+												'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
+												'<div class="view"> '+
+												'<img class= " " src="'+data.Biography[i].Poster +'" alt="">'+
+												'<div class="mask">'+
+												'<h2>'+ data.Biography[i].Title
+												+'</div>'
+												+'</h2>'
+												+'</div>' 
+												+'</div>' 
+												+'</div>'
+												+'</div>');
+				};
 
 })
