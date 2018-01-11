ac7454e5d2172def0d1bc997531dcd2e280e8e3a
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2018-01-11 10:57:38 -0300

    vista 3

---
+++ assets/js/app-vista3.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+ 
+ $('#send').click(function(){
+        var inputWrite = $('#inputWrite').val();
+        $('#printMessage').append('<div class="message">' + inputWrite + '</div>');
+        $('#inputWrite').val('');
+  });
+ 
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('#stars li').on('mouseover', function(){
+    console.log("hola");
+    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+   
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function(e){
+      if (e < onStar) {
+        $(this).addClass('hover');
+      }
+      else {
+        $(this).removeClass('hover');
+      }
+    });
+    
+  }).on('mouseout', function(){
+    $(this).parent().children('li.star').each(function(e){
+      $(this).removeClass('hover');
+    });
+  });
+  
+  
+  /* 2. Action to perform on click */
+  $('#stars li').on('click', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    var stars = $(this).parent().children('li.star');
+    
+    for (i = 0; i < stars.length; i++) {
+      $(stars[i]).removeClass('selected');
+    }
+    
+    for (i = 0; i < onStar; i++) {
+      $(stars[i]).addClass('selected');
+    }
+    
+});
--- assets/js/app.js
+++ assets/js/app.js
@@ -49,6 +49,15 @@ $(document).ready(function(){
       alert(error.code);
     });
   })
-  
+ 
+ $('#send').click(function(){
+        var inputWrite = $('#inputWrite').val();
+        $('#printMessage').append('<div class="message">' + inputWrite + '</div>');
+        $('#inputWrite').val('');
+  });
+ 
 
-})    
+}) 
+
+
+
