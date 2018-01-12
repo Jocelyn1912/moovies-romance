c8240ec5719ddde230fec3e715a30c087a7925a9
pinalef <tierralavista@gmail.com>
2018-01-11 21:29:47 -0300

    actualizando filtro

--- assets/js/vista2-app.js
+++ assets/js/vista2-app.js
@@ -1,57 +1,127 @@
 $(document).ready(function(){
+console.log(data);
 
-	console.log(data);
-	//llenando contenedores con peliculas del genero Drama
-	for (var i = 0 ;i < data.Drama.length ; i++){
-		$('.main').append('<div class="wrapper vista-first">' +
+	$('#dropdownMenu1').on('change', function(){
+			var selection = $('#dropdownMenu1').val();
+	
+
+			if(selection === 'Drama'){
+				$('.main').empty();
+				console.log('seleciono drama')
+			//llenando contenedores con peliculas del genero Drama
+				for (var i = 0 ;i < data.Drama.length ; i++){
+					$('.main').append('<div class="wrapper vista-first">' +
 											' <div class="row">'+
 											'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
 											'<div class="view"> '+
-											'<img class= " " src="'+data.Drama[i].Poster +'" alt="">'+
-											'<div class="mask">'+
-											'<h2>'+ data.Drama[i].Title
-											+'</div>'
+											'<img class= " " src="'+ data.Drama[i].Poster +'" alt="">'+
+											'<a href=""><div class="mask">'+
+											'<h2>'+ data.Drama[i].Title +
 											+'</h2>'
+											+'</div></a>'
 											+'</div>' 
 											+'</div>' 
 											+'</div>'
 											+'</div>');
-			};
+				};
+			}
 
-	
-	//llenando contenedores con peliculas del genero Comedy
-	for (var i = 0 ;i < data.Comedy.length ; i++){
-		$('.main').append('<div class="wrapper vista-first">' +
+			if(selection === 'Comedia'){
+				console.log('selecciono comedy');
+				$('.main').empty();
+					//llenando contenedores con peliculas del genero Comedy
+				for (var i = 0 ;i < data.Comedy.length ; i++){
+					$('.main').append('<div class="wrapper vista-first">' +
 											' <div class="row">'+
 											'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
 											'<div class="view"> '+
 											'<img class= " " src="'+data.Comedy[i].Poster +'" alt="">'+
-											'<div class="mask">'+
+											'<a href=""><div class="mask">'+
 											'<h2>'+ data.Comedy[i].Title
-											+'</div>'
 											+'</h2>'
+											+'</div></a>'
 											+'</div>' 
 											+'</div>' 
 											+'</div>'
 											+'</div>');
 			};
+			}
 
-	
-		//llenando contenedores con peliculas del genero Biography
-		for (var i = 0 ;i < data.Biography.length ; i++){
-			$('.main').append('<div class="wrapper vista-first">' +
+			if(selection === 'Biografia'){
+				console.log('selecciono biografia');
+				$('.main').empty();
+				//llenando contenedores con peliculas del genero Biography
+				for (var i = 0 ;i < data.Biography.length ; i++){
+					$('.main').append('<div class="wrapper vista-first">' +
 												' <div class="row">'+
 												'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
 												'<div class="view"> '+
 												'<img class= " " src="'+data.Biography[i].Poster +'" alt="">'+
-												'<div class="mask">'+
+												'<a href=""><div class="mask">'+
 												'<h2>'+ data.Biography[i].Title
-												+'</div>'
 												+'</h2>'
+												+'</div></a>'
 												+'</div>' 
 												+'</div>' 
 												+'</div>'
 												+'</div>');
 				};
+
+			}
+
+			if(selection === 'Familiar'){
+				console.log('selecciono familiar');
+				$('.main').empty();
+				//llenando contenedores con peliculas del genero Family
+				for (var i = 0 ;i < data.Family.length ; i++){
+					$('.main').append('<div class="wrapper vista-first">' +
+												' <div class="row">'+
+												'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
+												'<div class="view"> '+
+												'<img class= " " src="'+data.Family[i].Poster +'" alt="">'+
+												'<a href=""><div class="mask">'+
+												'<h2>'+ data.Family[i].Title
+												+'</h2>'
+												+'</div></a>'
+												+'</div>' 
+												+'</div>' 
+												+'</div>'
+												+'</div>');
+				};
+
+			}
+
+
+			if(selection === 'Fantasia'){
+				console.log('selecciono fantasia');
+				$('.main').empty();
+				//llenando contenedores con peliculas del genero Family
+				for (var i = 0 ;i < data.Fantasy.length ; i++){
+					console.log(data.Fantasy[i]);
+					$('.main').append('<div class="wrapper vista-first">' +
+												' <div class="row">'+
+												'<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">'+
+												'<div class="view"> '+
+												'<img class= " " src="'+data.Fantasy[i].Poster +'" alt="">'+
+												'<a href="#" class="a"><div class="mask">'+
+												'<h2>'+ data.Fantasy[i].Title
+												+'</h2>'
+												+'</div></a>'
+												+'</div>' 
+												+'</div>' 
+												+'</div>'
+												+'</div>');
+				};
+
+			}
+
+	})
+	
+	$('.mask').on('click', function(){
+	var text_val = $(this).children('h2').text();
+	console.log(text_val);
+		
+	})
+		
 
 })
